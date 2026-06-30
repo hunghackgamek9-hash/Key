64b319e629614dff8963db8f01079fcac00bff65
hunghackgamek9-hash <hunghackgamek9@gmail.com>
2026-06-30 17:14:13 +0700

    Update API.py

--- API.py
+++ API.py
@@ -38,7 +38,7 @@ def check():
         "expire": info["expire"]
     })
 
-if name == "main":
+if name == "__main__":
     app.run(
         host="0.0.0.0",
         port=int(os.environ.get("PORT", 10000))
